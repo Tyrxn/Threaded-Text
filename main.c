88d908a91fc0c346e90fff787fbcb1e1c8170856
Tyrxn <tyran.wt00@gmail.com>
2023-06-04 00:09:08 +1000

    Skeleton method for processing, frequency struct created

--- main.c
+++ main.c
@@ -3,10 +3,35 @@
 #include <string.h>
 #include <stdlib.h>
 
+typedef struct{
+    char* word;
+    int count;
+} word_freq;
 
 typedef struct{
     char* text;
     size_t text_length;
     size_t processed;
     pthread_mutex_t lock;
+    word_freq freq;
+    size_t num_words;
 } shm_t;
+
+void* process_text(void* arg){
+    shm_t* data = (shm_t*)arg;
+
+    while(1){
+        pthread_mutex_lock(&data->lock);
+
+        if(data->processed >= data->text_length){
+            pthread_mutex_unlock(&data->lock);
+            return NULL;
+        }
+
+        
+
+        pthread_mutex_unlock(&data->lock);
+
+
+    }
+}
